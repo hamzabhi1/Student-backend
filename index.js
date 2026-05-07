eb2f2764a7e6971006afcb00895e9d018687eee5
Hamza <hamzabhilegend002@gmail.com>
2026-05-07 08:32:55 -0700

    fdfd

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import connectDB from "./config/db.js";
-import mongoose from "mongoose";
 
 const app = express();
 
@@ -10,44 +9,27 @@ connectDB();
 
 app.use(
   cors({
-    origin: "https://student-frontend-d3l9p7yj2-hamzabhi1s-projects.vercel.app",
+    origin: [
+      "https://student-frontend-aoqy6lhbd-hamzabhi1s-projects.vercel.app",
+      "https://student-frontend-d3l9p7yj2-hamzabhi1s-projects.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   }),
 );
 
 // Parse middleware MUST come before routes
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 
-const isConnected = false;
-
-async function connectToMongoDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-}
-app.use((req, res, next) => {
-  if (!isConnected) {
-    connectToMongoDB();
-  }
-  next();
-});
-
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-// Export a serverless-compatible handler for Vercel
+
+// Export handler for Vercel
 export default function handler(req, res) {
   return app(req, res);
 }
-module.exports = app;
