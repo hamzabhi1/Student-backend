878a7378921fd251fe1522701a172552065afd0d
Hamza <hamzabhilegend002@gmail.com>
2026-05-07 09:55:06 -0700

    dfdfdfsdfdsf

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import serverless from "serverless-http";
-import authRoutes from "./routes/authRoutes.js";
-import connectDB from "./config/db.js";
+import authRoutes from "../routes/authRoutes.js";
+import connectDB from "../config/db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 
@@ -11,22 +13,17 @@ connectDB();
 
 app.use(cors({
   origin: [
-    "https://student-frontend-aoqy6lhbd-hamzabhi1s-projects.vercel.app",
-    "https://student-frontend-d3l9p7yj2-hamzabhi1s-projects.vercel.app",
     "http://localhost:5173"
   ],
   credentials: true
 }));
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
 
-app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// ⭐ IMPORTANT FIX
 export default serverless(app);
