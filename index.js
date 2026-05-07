1b28a9813f6fbac4755bc317e6d1ad039317c149
Hamza <hamzabhilegend002@gmail.com>
2026-05-07 07:38:13 -0700

    dfdg

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import connectDB from "./config/db.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -21,6 +22,27 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 
+const isConnected = false;
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectToMongoDB();
+  }
+  next();
+});
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -28,3 +50,4 @@ app.get("/", (req, res) => {
 export default function handler(req, res) {
   return app(req, res);
 }
+module.exports = app;
